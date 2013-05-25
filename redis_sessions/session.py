0f93c7875df9fb242b7c3c76217cc56b5d2b63e7
condor <condor@condor-VirtualBox.(none)>
2013-05-26 01:21:25 +0200

    Default session engine but redis store undecoded session dict

--- redis_sessions/session.py
+++ redis_sessions/session.py
@@ -5,6 +5,13 @@ except ImportError:  # Python 3.*
     from django.utils.encoding import force_text as force_unicode
 from django.contrib.sessions.backends.base import SessionBase, CreateError
 from redis_sessions import settings
+import pickle, base64
+
+from django.contrib.sessions.backends.base import SessionBase, CreateError
+from django.core.exceptions import SuspiciousOperation
+from django.db import IntegrityError, transaction, router
+from django.utils import timezone
+
 
 
 # Avoid new redis connection on each request
@@ -40,42 +47,61 @@ class SessionStore(SessionBase):
 
     def load(self):
         try:
-            session_data = self.server.get(
-                self.get_real_stored_key(self._get_or_create_session_key())
+            s = Session.objects.get(
+                session_key = self.session_key,
+                expire_date__gt=timezone.now()
             )
-            return self.decode(force_unicode(session_data))
-        except:
+            return self.decode(force_unicode(s.session_data))
+        except (Session.DoesNotExist, SuspiciousOperation):
             self.create()
             return {}
-
+        
     def exists(self, session_key):
-        return self.server.exists(self.get_real_stored_key(session_key))
+        return Session.objects.filter(session_key=session_key).exists()
+        
 
     def create(self):
         while True:
             self._session_key = self._get_new_session_key()
-
             try:
+                # Save immediately to ensure we have a unique entry in the
+                # database.
                 self.save(must_create=True)
             except CreateError:
+                # Key wasn't unique. Try again.
                 continue
             self.modified = True
+            self._session_cache = {}
             return
 
     def save(self, must_create=False):
-        if must_create and self.exists(self._get_or_create_session_key()):
-            raise CreateError
+
+        obj = Session(
+            session_key=self._get_or_create_session_key(),
+            session_data=self.encode(self._get_session(no_load=must_create)),
+            expire_date=self.get_expiry_date()
+        )
+        using = router.db_for_write(Session, instance=obj)
+        sid = transaction.savepoint(using=using)
+        try:
+            obj.save(force_insert=must_create, using=using)
+        except IntegrityError:
+            if must_create:
+                transaction.savepoint_rollback(sid, using=using)
+                raise CreateError
+            raise
+
         data = self.encode(self._get_session(no_load=must_create))
         if redis.VERSION[0] >= 2:
             self.server.setex(
                 self.get_real_stored_key(self._get_or_create_session_key()),
                 self.get_expiry_age(),
-                data
+                pickle.loads(base64.decodestring(data).split(":",1)[1])
             )
         else:
             self.server.set(
                 self.get_real_stored_key(self._get_or_create_session_key()),
-                data
+                pickle.loads(base64.decodestring(data).split(":",1)[1])
             )
             self.server.expire(
                 self.get_real_stored_key(self._get_or_create_session_key()),
@@ -88,8 +114,9 @@ class SessionStore(SessionBase):
                 return
             session_key = self.session_key
         try:
+            Session.objects.get(session_key=session_key).delete()
             self.server.delete(self.get_real_stored_key(session_key))
-        except:
+        except Session.DoesNotExist:
             pass
 
     def get_real_stored_key(self, session_key):
@@ -100,3 +127,6 @@ class SessionStore(SessionBase):
         if not prefix:
             return session_key
         return ':'.join([prefix, session_key])
+
+# At bottom to avoid circular import
+from django.contrib.sessions.models import Session
